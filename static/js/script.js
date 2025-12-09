aee62032ce3c23203be1ef83d7435fc2b8aa2be3
Lipezxx2 <felipeferreira405060@gmail.com>
2025-12-09 11:47:34 -0300

    agendamento

--- static/js/script.js
+++ static/js/script.js
@@ -17,3 +17,27 @@ function abrirFormulario() {
             form.style.display = "none";
         }
     }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const campoData = document.querySelector('input[type="date"]');
+
+    const hoje = new Date();
+    const ano = hoje.getFullYear();
+    const mes = String(hoje.getMonth() + 1).padStart(2, '0');
+    const dia = String(hoje.getDate()).padStart(2, '0');
+
+    const dataMinima = `${ano}-${mes}-${dia}`;
+
+   
+    campoData.min = dataMinima;
+
+    
+    campoData.addEventListener("change", function () {
+        if (campoData.value < dataMinima) {
+            alert("Essa data já passou");
+            campoData.value = "";
+        }
+    });
+});
+
